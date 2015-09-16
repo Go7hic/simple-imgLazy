6ddbdde4156ef38d9ad5160d0cfdea2f79ae30a4
Go7hic <gtfx0209@qq.com>
2015-09-17 00:07:58 +0800

    bugfix

--- simple-ImgLazy.js
+++ simple-ImgLazy.js
@@ -1,67 +1,73 @@
-!function(window){
-          var $q = function(q, res){
-                if (document.querySelectorAll) {
-                  res = document.querySelectorAll(q);
-                } else {
-                  var d=document,
-                      a=d.styleSheets[0] || d.createStyleSheet();
-                  a.addRule(q,'f:b');
-                  for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
-                    l[b].currentStyle.f && c.push(l[b]);
+/**
+ * simple img lazy loading
+ * @author  Go7hic
+ * @param  {[type]} window [description]
+ * @return {[type]}        [description]
+ */
+;function(window){
+    var imgDom = function(dom, res) {
+        if (document.querySelectorAll) {
+            res = document.querySelectorAll(dom);
+        } else {
+            var d = document,
+            a = d.styleSheets[0] || d.createStyleSheet();
+            a.addRule(dom,'f:b');
+            for(var l=d.all,b=0,c=[],f=l.length;b<f;b++) {
+                l[b].currentStyle.f && c.push(l[b]);
+            }
+                a.removeRule(0);
+                res = c;
+        }
+        return res;
+    },
+    addEventListener = function(evt, fn) {
+        window.addEventListener
+        ? this.addEventListener(evt, fn, false)
+        : (window.attachEvent)
+        ? this.attachEvent('on' + evt, fn)
+        : this['on' + evt] = fn;
+    },
+    _has = function(obj, key) {
+        return Object.prototype.hasOwnProperty.call(obj, key);
+    };
 
-                  a.removeRule(0);
-                  res = c;
-                }
-                return res;
-              },
-            addEventListener = function(evt, fn){
-                window.addEventListener
-                  ? this.addEventListener(evt, fn, false)
-                  : (window.attachEvent)
-                    ? this.attachEvent('on' + evt, fn)
-                    : this['on' + evt] = fn;
-              },
-            _has = function(obj, key) {
-                return Object.prototype.hasOwnProperty.call(obj, key);
-              };
-
-          function loadImage (el, fn) {
-            var img = new Image(),
-                src = el.getAttribute('data-src');
-            img.onload = function() {
-              if (!! el.parent)
+    function loadImage (el, fn) {
+        var img = new Image(),
+            src = el.getAttribute('data-src');
+        img.onload = function() {
+            if (!! el.parent)
                 el.parent.replaceChild(img, el)
-              else
+            else
                 el.src = src;
 
-              fn? fn() : null;
+            fn? fn() : null;
+        }
+        img.src = src;
+    }
+
+    function elementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+         rect.top    >= 0
+         && rect.left   >= 0
+         && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+        )
+    }
+    var images = new Array(),
+        query = imgDom('.imglazy'),
+        processScroll = function(){
+            for (var i = 0; i < images.length; i++) {
+                if (elementInViewport(images[i])) {
+                    loadImage(images[i], function () {
+                    images.splice(i, i);
+                });
             }
-            img.src = src;
-          }
+        };
+    };
+    for (var i = 0; i < query.length; i++) {
+        images.push(query[i]);
+    };
 
-          function elementInViewport(el) {
-            var rect = el.getBoundingClientRect();
-            return (
-               rect.top    >= 0
-            && rect.left   >= 0
-            && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-            )
-          }
-            var images = new Array(),
-                query = $q('.imglazy'),
-                processScroll = function(){
-                  for (var i = 0; i < images.length; i++) {
-                    if (elementInViewport(images[i])) {
-                      loadImage(images[i], function () {
-                        images.splice(i, i);
-                      });
-                    }
-                  };
-                };
-            for (var i = 0; i < query.length; i++) {
-              images.push(query[i]);
-            };
-
-            processScroll();
-            addEventListener('scroll',processScroll);
-        }(this);
+    processScroll();
+    addEventListener('scroll',processScroll);
+}(this);
